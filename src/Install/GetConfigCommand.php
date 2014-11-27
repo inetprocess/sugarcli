b4d90dfaf00184db865b29e3d6af502ed945bd04
Rémi SAUVAT <remi.sauvat@inetprocess.com>
2014-11-27 17:34:17 +0100

    Add command to get a default install config_si.php file.

---
+++ src/Install/GetConfigCommand.php
@@ -0,0 +1,42 @@
+<?php
+namespace SugarCli\Install;
+/*
+ * Check command to verify that Sugar is present and installed.
+ */
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class GetConfigCommand extends Command 
+{
+    protected function configure()
+    {
+        $this->setName("install:config:get")
+            ->setDescription('Write a default config_si.php file in the current folder')
+            ->addOption(
+                'config',
+                'c',
+                InputOption::VALUE_REQUIRED,
+                'Write to this file instead of config_si.php.',
+                'config_si.php')
+            ->addOption(
+                'force',
+                'f',
+                InputOption::VALUE_NONE,
+                'Overwrite existing file');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $fs = new Filesystem();
+        $fs->copy(
+            ROOT_DIR . '/files/config_si.php',
+            $input->getOption('config'),
+            $input->getOption('force')
+        );
+    }
+}
