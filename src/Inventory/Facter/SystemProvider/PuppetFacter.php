f69a6010e3a9594fd316984167367a751e69a5ee
Rémi Sauvat <remi.sauvat@inetprocess.com>
2015-09-04 17:15:33 +0200

    Filter facts return by PuppetFacter.

--- src/Inventory/Facter/SystemProvider/PuppetFacter.php
+++ src/Inventory/Facter/SystemProvider/PuppetFacter.php
@@ -4,13 +4,38 @@ namespace SugarCli\Inventory\Facter\SystemProvider;
 
 use SugarCli\Inventory\Facter\CommandProvider;
 
-/**
- * @todo Filter on return keys.
- */
 class PuppetFacter extends CommandProvider
 {
+    protected $facts_keys = array(
+        'architecture',
+        'domain',
+        'fqdn',
+        'ipaddress',
+        'memoryfree',
+        'memoryfree_mb',
+        'memorysize',
+        'memorysize_mb',
+        'os',
+        'processors',
+        'swapfree',
+        'swapfree_mb',
+        'swapsize',
+        'swapsize_mb',
+        'system_uptime',
+        'timezone',
+    );
+
     public function __construct()
     {
         parent::__construct('facter --json', true);
+    }
+
+    /**
+     * Filter results from facter
+     */
+    public function getFacts()
+    {
+        $facts = parent::getFacts();
+        return array_intersect_key($facts, array_flip($this->facts_keys));
     }
 }
