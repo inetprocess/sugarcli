2d7cc2b76c096996f97e2b5d802a5f559095c86e
Rémi Sauvat <remi.sauvat@inetprocess.com>
2015-09-23 18:03:35 +0200

    Add fqdn to Hostname provider for inventory.

--- src/Inventory/Facter/SystemProvider/Hostname.php
+++ src/Inventory/Facter/SystemProvider/Hostname.php
@@ -2,12 +2,19 @@
 
 namespace SugarCli\Inventory\Facter\SystemProvider;
 
+use Symfony\Component\Process\Process;
 use SugarCli\Inventory\Facter\FacterInterface;
 
 class Hostname implements FacterInterface
 {
     public function getFacts()
     {
-        return array('hostname' => gethostname());
+        $process = new Process('hostname --fqdn');
+        $process->mustRun();
+        $fqdn = trim($process->getOutput());
+        return array(
+            'fqdn' => $fqdn,
+            'hostname' => gethostname()
+        );
     }
 }
