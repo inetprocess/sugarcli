179e30fdb63d9ab75da2931b37db62f81fb3391a
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2015-12-02 15:57:47 +0100

    Added a new method to get hooks

--- src/Console/Command/HooksListCommand.php
+++ src/Console/Command/HooksListCommand.php
@@ -13,9 +13,6 @@ use Inet\SugarCRM\LogicHook;
 
 class HooksListCommand extends AbstractConfigOptionCommand
 {
-    const BOOL_TRUE = "\xE2\x9C\x94";
-    const BOOL_FALSE = "\xE2\x9C\x95";
-
     protected function configure()
     {
         $this->setName('hooks:list')
@@ -57,13 +54,11 @@ class HooksListCommand extends AbstractConfigOptionCommand
 
         $headers = array(
             array(
-                new TableCell(
-                    PHP_EOL . "<comment>Hooks definition for $module</comment>" . PHP_EOL,
-                    array('colspan' => count($colsName))
-                )
+                new TableCell("<comment>Hooks definition for $module</comment>", array('colspan' => count($colsName)))
             ),
             $colsName,
         );
+
         try {
             $table = new Table($output);
             // Output table
@@ -72,43 +67,55 @@ class HooksListCommand extends AbstractConfigOptionCommand
             $tableData = array();
             $logicHook = new LogicHook($sugarEP);
             $hooksList = $logicHook->getModuleHooks($module);
-            foreach ($hooksList as $hookType => $hooks) {
-                $tableData[] = array(
-                    new TableCell(
-                        "<comment>--- Hook $hookType --- </comment>",
-                        array('colspan' => count($colsName))
-                    )
-                );
-                foreach ($hooks as $hook) {
-                    // New line every 5 words
-                    $words = explode(' ', $hook['Description']);
-                    for ($i = 0; $i < count($words); $i++) {
-                        if ($i !== 0 && $i%5 === 0) {
-                            $words[$i] = PHP_EOL . $words[$i];
-                        }
-                    }
-                    $hook['Description'] = implode(' ', $words);
-
-                    // Remove useless fields if in compact mode
-                    if ($compact) {
-                        unset($hook['File']);
-                        unset($hook['Class']);
-                    }
-
-                    $tableData[] = array_values($hook);
-                }
-                $tableData[] = new TableSeparator();
-            }
-
-            // Destroy last line
-            unset($tableData[count($tableData) - 1]);
-
-            $table->setRows($tableData);
-            $table->render();
         } catch (BeanNotFoundException $e) {
             $msg = "Unknown module '$module'. Valid modules are:" . PHP_EOL;
             $msg.= '    - ' . implode(PHP_EOL . '    - ', $validModules);
             throw new \InvalidArgumentException($msg);
         }
+
+        $hooksComs = $logicHook->getModulesLogicHooksDef();
+        if (empty($hooksList)) {
+            $tableData[] = array(
+                new TableCell("<error>No Hooks for that module</error>", array('colspan' => count($colsName)))
+            );
+        }
+
+        $procHooks = 0;
+        $nbHooks = count($hooksList);
+        foreach ($hooksList as $type => $hooks) {
+            $com = '';
+            if (array_key_exists($type, $hooksComs)) {
+                $com = $hooksComs[$type];
+            }
+            $tableData[] = array(
+                new TableCell("<comment>$type ($com)</comment>", array('colspan' => count($colsName)))
+            );
+            foreach ($hooks as $hook) {
+                // New line every 5 words
+                $words = explode(' ', $hook['Description']);
+                for ($i = 0; $i < count($words); $i++) {
+                    $words[$i] = ($i !== 0 && $i%5 === 0 ? PHP_EOL : '') . $words[$i];
+                }
+                $hook['Description'] = implode(' ', $words);
+
+                // Remove useless fields if in compact mode
+                if ($compact) {
+                    unset($hook['File']);
+                    unset($hook['Class']);
+                }
+
+                $tableData[] = array_values($hook);
+            }
+
+            // Create a separator if I am not
+            $procHooks++;
+            if ($procHooks < $nbHooks) {
+                $tableData[] = new TableSeparator();
+            }
+
+        }
+
+        $table->setRows($tableData);
+        $table->render();
     }
 }
---
+++ tests/Console/Command/HooksListCommandTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace SugarCli\Tests\Console\Command;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\Console\Tester\CommandTester;
+use Psr\Log\NullLogger;
+
+use Inet\SugarCRM\Application as SugarApp;
+use Inet\SugarCRM\EntryPoint;
+use SugarCli\Console\Application;
+
+class HooksListCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function getEntryPointInstance()
+    {
+        if (!EntryPoint::isCreated()) {
+            $logger = new NullLogger;
+            EntryPoint::createInstance(
+                new SugarApp($logger, getenv('SUGARCLI_SUGAR_PATH')),
+                '1'
+            );
+            $this->assertInstanceOf('Inet\SugarCRM\EntryPoint', EntryPoint::getInstance());
+        }
+        return EntryPoint::getInstance();
+    }
+
+    public function getCommandTester($cmd_name = 'hooks:list')
+    {
+        $app = new Application();
+        $app->configure(
+            new ArrayInput(array()),
+            new StreamOutput(fopen('php://memory', 'w', false))
+        );
+        $app->setEntryPoint($this->getEntryPointInstance());
+        $cmd = $app->find($cmd_name);
+
+        return new CommandTester($cmd);
+    }
+
+    /** Missing Param module: exception thrown
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessageRegExp #You must define the module with --module#
+     */
+    public function testListMissingParam()
+    {
+        $cmd = $this->getCommandTester();
+        $cmd->execute(array(
+            '--path' => getenv('SUGARCLI_SUGAR_PATH'),
+        ));
+    }
+
+    /** Define a wrong module: exception thrown
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessageRegExp #.*Unknown module 'TOTO'.*#
+     */
+    public function testListWrongParam()
+    {
+        $cmd = $this->getCommandTester();
+        $cmd->execute(array(
+            '--path' => getenv('SUGARCLI_SUGAR_PATH'),
+            '--module' => 'TOTO',
+        ));
+    }
+
+    public function testListHookRightModule()
+    {
+        $cmd = $this->getCommandTester();
+        $result = $cmd->execute(array(
+            '--path' => getenv('SUGARCLI_SUGAR_PATH'),
+            '--module' => 'Meetings'
+        ));
+        $output = $cmd->getDisplay();
+        $this->assertEquals(0, $cmd->getStatusCode());
+        $msg = 'Check that your Sugar instance has the default Hook before_relationship_update for Meetings';
+        $this->assertContains('Hooks definition for Meetings', $output, $msg);
+        $this->assertContains('before_relationship_update', $output, $msg);
+    }
+
+    public function testListHookRightModuleEmptyHooks()
+    {
+        $cmd = $this->getCommandTester();
+        $result = $cmd->execute(array(
+            '--path' => getenv('SUGARCLI_SUGAR_PATH'),
+            '--module' => 'Leads'
+        ));
+        $output = $cmd->getDisplay();
+        $this->assertEquals(0, $cmd->getStatusCode());
+        $msg = 'Check that your Sugar instance has no Hooks for Leads';
+        $this->assertContains('Hooks definition for Leads', $output, $msg);
+        $this->assertContains('No Hooks for that module', $output, $msg);
+    }
+}
