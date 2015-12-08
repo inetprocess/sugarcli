8d867fdaa16fbd0e583abec3499470342bcd3232
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2015-12-08 14:56:57 +0100

    We can now manage the DB with QRR

--- src/Console/Command/SystemQuickRepairCommand.php
+++ src/Console/Command/SystemQuickRepairCommand.php
@@ -3,31 +3,65 @@
 namespace SugarCli\Console\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\ProgressIndicator;
 use Inet\SugarCRM\System as SugarSystem;
 
 class SystemQuickRepairCommand extends AbstractConfigOptionCommand
 {
+    protected $messages = array();
+
     protected function configure()
     {
         $this->setName('system:quickrepair')
-            ->setDescription('Do a quick repair and rebuild.')
-            ->addConfigOptionMapping('path', 'sugarcrm.path');
+             ->setDescription('Do a quick repair and rebuild.')
+             ->addConfigOptionMapping('path', 'sugarcrm.path')
+             ->addOption(
+                 'database',
+                 'd',
+                 InputOption::VALUE_NONE,
+                 'Manage database changes.'
+             )
+             ->addOption(
+                 'force',
+                 'f',
+                 InputOption::VALUE_NONE,
+                 'Really execute the SQL queries (displayed by using -v).'
+             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $logger = $this->getService('logger');
         $this->setSugarPath($this->getConfigOption($input, 'path'));
         $sugarEP = $this->getService('sugarcrm.entrypoint');
 
+        $output->writeln('<comment>Reparation</comment>: ');
         $progress = new ProgressIndicator($output);
         $progress->start('Starting...');
         $progress->advance();
         $sugarSystem = new SugarSystem($sugarEP);
         $progress->setMessage('Working...');
-        $sugarSystem->repair();
-        $progress->finish('<comment>Repair Done.</comment>');
+        $messages = $sugarSystem->repair($input->getOption('force'), true);
+        $progress->finish('<info>Repair Done.</info>');
+
+        if ($input->getOption('database') === false) {
+            return;
+        }
+
+        $output->writeln(PHP_EOL . '<comment>Database Messages</comment>: ');
+        // We have something to sync
+        if (strpos($messages[1], 'Database tables are synced with vardefs') !== 0) {
+            if ($input->getOption('force') === false) {
+                $output->writeln($messages[1]);
+                $output->writeln(PHP_EOL . '<error>You need to use --force to run the queries</error>');
+            } else {
+                $output->writeln('<info>Queries run, try another repair to verify</info>');
+            }
+        // Nothing to sync, default sugar message
+        } else {
+            $output->writeln($messages[1]);
+        }
     }
 }
