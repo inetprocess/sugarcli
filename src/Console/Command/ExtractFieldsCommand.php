1c46e47d7997295a04355567767a78811c430179
Rémi Sauvat <remi.sauvat@inetprocess.com>
2016-03-29 13:55:18 +0200

    Make sensiolabs happy: remove unused 'use' statement.

--- src/Console/Command/ExtractFieldsCommand.php
+++ src/Console/Command/ExtractFieldsCommand.php
@@ -20,7 +20,6 @@ namespace SugarCli\Console\Command;
 
 use CSanquer\ColibriCsv\CsvWriter;
 use Inet\SugarCRM\Bean as BeanManager;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
