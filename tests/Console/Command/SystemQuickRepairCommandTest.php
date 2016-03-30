164718efa9e3700451ce6439ef62420d1bb98450
Rémi Sauvat <remi.sauvat@inetprocess.com>
2016-03-30 15:03:18 +0200

    Add test for system:quickrepair

--- tests/Console/Command/SystemQuickRepairCommandTest.php
+++ tests/Console/Command/SystemQuickRepairCommandTest.php
@@ -4,6 +4,7 @@ namespace SugarCli\Tests\Console\Command;
 
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tester\CommandTester;
 use Psr\Log\NullLogger;
 
@@ -42,22 +43,24 @@ class SystemQuickRepairCommandTest extends \PHPUnit_Framework_TestCase
         return new CommandTester($cmd);
     }
 
+    /**
+     * @group sugarcrm-slow
+     */
     public function testRepair()
     {
         $cmd = $this->getCommandTester();
 
-        /* THE FOLLOWING IS NOT WORKING BECAUSE SUGAR DOES VERY STRANGE THINGS
         $checkFile = getenv('SUGARCLI_SUGAR_PATH') . '/cache/class_map.php';
         $this->assertFileExists($checkFile, 'That file is used to test my repair');
         unlink($checkFile);
         $this->assertFileNotExists($checkFile, 'That file is used to test my repair');
-        $result = $cmd->execute(array(
-            '--path' => getenv('SUGARCLI_SUGAR_PATH')
-        ));
+        $result = $cmd->execute(
+            array('--path' => getenv('SUGARCLI_SUGAR_PATH')),
+            array('verbosity' => OutputInterface::VERBOSITY_VERBOSE)
+        );
         $output = $cmd->getDisplay();
         $this->assertEquals(0, $cmd->getStatusCode());
-        $this->assertEmpty($output);
+        $this->assertNotEmpty($output);
         $this->assertFileExists($checkFile, 'That file is used to test my repair');
-        */
     }
 }
