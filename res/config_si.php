df32205e57d233f53787159dbd6855ca175e1dd4
Rémi Sauvat <remi.sauvat@inetprocess.com>
2016-03-31 16:19:40 +0200

    Make a prettier config_si.php for installation.

--- res/config_si.php
+++ res/config_si.php
@@ -1,46 +1,74 @@
 <?php
 
-$sugar_config_si = array(
-    'setup_db_host_name' => 'localhost',
-    'setup_db_sugarsales_user' => '<DB_USER>',
-    'setup_db_sugarsales_password' => '<DB_PASSWORD>',
-    'setup_db_database_name' => '<DB_NAME>',
-    'setup_db_type' => 'mysql',
-    'setup_db_pop_demo_data' => false,
+// Database configuration
+$sugar_config_si['setup_db_sugarsales_user'] = '<DB_USER>';
+$sugar_config_si['setup_db_sugarsales_password'] = '<DB_PASSWORD>';
+$sugar_config_si['setup_db_database_name'] = '<DB_NAME>';
+$sugar_config_si['setup_db_host_name'] = 'localhost';
 
-    'setup_db_create_database' => 1,
-    'setup_db_create_sugarsales_user' => false,
-    'setup_db_drop_tables' => 0,
-    'setup_db_username_is_privileged' => true,
-    'demoData' => 'en_us',
+// Elastic search configuration
+$sugar_config_si['setup_fts_type'] = 'Elastic';
+$sugar_config_si['setup_fts_host'] = 'localhost';
+$sugar_config_si['setup_fts_port'] = '9200';
 
-    'setup_db_admin_user_name' => '<DB_ADMIN_USER>',
-    'setup_db_admin_password' => '<DB_ADMIN_PASSWORD>',
+// Sugar Config
+$sugar_config_si['setup_site_url'] = '<SITE_URL>';
+$sugar_config_si['setup_site_admin_user_name'] = '<SUGAR_ADMIN_USER>';
+$sugar_config_si['setup_site_admin_password'] = '<SUGAR_ADMIN_PASSWORD>';
+$sugar_config_si['setup_license_key'] = '<SUGAR_LICENSE>';
 
-    'setup_fts_type' => 'Elastic',
-    'setup_fts_host' => 'localhost',
-    'setup_fts_port' => '9200',
+// Install demo data <yes|no>
+$sugar_config_si['demoData'] = 'no';
 
-    'setup_site_url' => '<SITE_URL>',
-    'setup_site_admin_user_name'=>'<SUGAR_ADMIN_USER>',
-    'setup_site_admin_password' => '<SUGAR_ADMIN_PASSWORD>',
-    'setup_license_key' => '<SUGAR_LICENSE>',
+// Default system configuration
+$sugar_config_si['setup_system_name'] = 'SugarCRM - Commercial CRM';
+// English (US)
+$sugar_config_si['default_language'] = 'en_us';
+$sugar_config_si['default_locale_name_format'] = 's f l';
 
-    'setup_site_sugarbeet_automatic_checks' => true,
+$sugar_config_si['default_currency_iso4217'] = 'USD';
+$sugar_config_si['default_currency_name'] = 'US Dollar';
+$sugar_config_si['default_currency_symbol'] = '$';
+$sugar_config_si['default_currency_significant_digits'] = '2';
 
-    'default_currency_iso4217' => 'USD',
-    'default_currency_name' => 'US Dollars',
-    'default_currency_significant_digits' => '2',
-    'default_currency_symbol' => '$',
-    'default_date_format' => 'Y-m-d',
-    'default_time_format' => 'H:i',
-    'default_decimal_seperator' => '.',
-    'default_export_charset' => 'UTF-8',
-    'default_language' => 'en_us',
-    'default_locale_name_format' => 's f l',
-    'default_number_grouping_seperator' => ',',
-    'export_delimiter' => ',',
+$sugar_config_si['default_decimal_seperator'] = '.';
+$sugar_config_si['default_number_grouping_seperator'] = ',';
 
-    'setup_system_name' => 'SugarCRM - Commercial CRM',
-);
+$sugar_config_si['default_date_format'] = 'm/d/Y';
+$sugar_config_si['default_time_format'] = 'h:ia';
 
+$sugar_config_si['default_export_charset'] = 'UTF-8';
+$sugar_config_si['export_delimiter'] = ',';
+
+
+/* French configuration */
+// $sugar_config_si['default_language'] = 'fr_FR';
+// $sugar_config_si['default_locale_name_format'] = 's f l';
+//
+// $sugar_config_si['default_currency_iso4217'] = 'EUR';
+// $sugar_config_si['default_currency_name'] = 'Euro';
+// $sugar_config_si['default_currency_symbol'] = '€';
+// $sugar_config_si['default_currency_significant_digits'] = '2';
+//
+// $sugar_config_si['default_decimal_seperator'] = ',';
+// $sugar_config_si['default_number_grouping_seperator'] = ',';
+//
+// $sugar_config_si['default_date_format'] = 'd/m/Y';
+// $sugar_config_si['default_time_format'] = 'H:i';
+//
+// $sugar_config_si['default_export_charset'] = 'UTF-8';
+// $sugar_config_si['export_delimiter'] = ',';
+
+
+// Advanced configuration
+$sugar_config_si['setup_db_type'] = 'mysql';
+$sugar_config_si['setup_db_pop_demo_data'] = false;
+
+$sugar_config_si['setup_db_create_database'] = 1;
+$sugar_config_si['setup_db_create_sugarsales_user'] = false;
+$sugar_config_si['setup_db_drop_tables'] = 0;
+$sugar_config_si['setup_db_username_is_privileged'] = true;
+$sugar_config_si['setup_db_admin_user_name'] = $sugar_config_si['setup_db_sugarsales_user'];
+$sugar_config_si['setup_db_admin_password'] = $sugar_config_si['setup_db_sugarsales_password'];
+
+$sugar_config_si['setup_site_sugarbeet_automatic_checks'] = true;
