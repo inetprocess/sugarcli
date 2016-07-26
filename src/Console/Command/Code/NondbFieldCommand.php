31a8e982cdbf77340ce43860c7ed10cc11257c09
Joe Cora <joseph.cora@nytimes.com>
2016-07-26 13:03:39 -0400

    WIP. Support for non-db fields related to submodules

---
+++ src/Console/Command/Code/NondbFieldCommand.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * SugarCLI
+ *
+ * PHP Version 5.3 -> 5.5
+ * SugarCRM Versions 6.5 - 7.7
+ *
+ * @author Joe Cora
+ * @copyright 2016 The New York Times
+ *
+ * @package nyt/sugarcli-nyt
+ *
+ * @license Apache License 2.0
+ */
+
+namespace SugarCli\Console\Command\Code;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use SugarCli\Console\Command\AbstractConfigOptionCommand;
+use SugarCli\Console\Templater;
+use SugarCli\Console\TemplateTypeEnum;
+use SugarCli\Utils\CodeCommandsUtility;
+use SugarCli\Utils\Utils;
+
+class NondbFieldCommand extends AbstractConfigOptionCommand
+{
+    // Class members /////////////////////////////////////////////////////
+    /**
+     * Store Options values
+     *
+     * @var array $options
+     */
+    protected $options = array();
+
+    // Class methods /////////////////////////////////////////////////////
+    /**
+     * Configure the command
+     */
+    protected function configure()
+    {
+        // Configure the command with its name and options
+        $this->setName('code:nondbField')
+            ->setDescription('Add the skeleton code for a non-db field that is referenced within a submodule')
+            ->addConfigOptionMapping('path', 'sugarcrm.path')
+            ->addOption(
+                'module',
+                'm',
+                InputOption::VALUE_REQUIRED,
+                'Module Name for Non-DB Fields'
+            )
+            ->addOption(
+                'related',
+                'r',
+                InputOption::VALUE_REQUIRED,
+                'Related Submodule Name'
+            )
+            ->addOption(
+                'name',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Related Field Name'
+            );
+    }
+
+    /**
+     * Run the command
+     *
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Set the Sugar path from one of the specified locations and verify commandline options
+        $this->setSugarPath($this->getConfigOption($input, 'path'));
+        $this->checkOptions($input);
+
+        // Prepare replacement values array for template writing
+        $replacements = array(
+            'module' => $this->options['module'],
+            'moduleBase' => Utils::baseModuleName($this->options['module']),
+            'field' => $this->options['name'],
+            'type' => $this->options['type']
+        );
+
+        // Retrieve the templater service from app container
+        /** @var Templater $templater */
+        $templater = $this->getContainer()->get('templater');
+
+        // Process and write the files from the templates for a field
+        $templateWriter = new CodeCommandsUtility($templater);
+
+        $templateWriter->writeFilesFromTemplatesForType($replacements, TemplateTypeEnum::FIELD,
+            $this->getService('sugarcrm.entrypoint')->getPath());
+
+        // Output success message
+        $output->writeln('Files for custom '. $this->options['type']. ' field, '. $this->options['name']. ', for module, '. $this->options['module']. ', added.');
+        
+        // Everything went fine
+        return 0;
+    }
+
+    /**
+     * Check required options and their values
+     *
+     * @param InputInterface $input
+     */
+    protected function checkOptions(InputInterface $input)
+    {
+        // Confirm that the module name exists
+        $this->options['module'] = $input->getOption('module');
+
+        if (empty($this->options['module'])) {
+            throw new \InvalidArgumentException('You must define the module\'s name');
+        }
+
+        // Confirm that the field name exists
+        $this->options['name'] = $input->getOption('name');
+
+        if (empty($this->options['name'])) {
+            throw new \InvalidArgumentException('You must define the new field\'s name');
+        }
+
+        // Confirm that the field type is available
+        $this->options['type'] = $input->getOption('type');
+
+        if (!in_array($this->options['type'], self::$fieldtypes)) {
+            $errorMsg  = 'You must define a valid field type';
+            $errorMsg .= PHP_EOL . PHP_EOL . 'Available field types are:';
+            $errorMsg .= PHP_EOL. "\t". join(PHP_EOL. "\t", self::$fieldtypes);
+
+            throw new \InvalidArgumentException($errorMsg);
+        }
+    }
+}
